664d0faf42b0f97f17c55a57d295c52ea99b0362
Kelly Robson <kellyalvesrobson@gmail.com>
2025-10-14 14:35:20 -0300

    finish schemas UserIn and UserOut

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+
+
 app = FastAPI()
 
 @app.get("/users")
 async def add_new_user():
-    pass
+    
---
+++ schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel,EmailStr
+
+class UserIn(BaseModel):
+    username:str
+    email:EmailStr
+    password:str
+
+class UserOut(BaseModel):
+    id:int | None
+    username:str
+    email:str   
