0ce1aaedc24fd57050af32e0e3d20f336a34c891
Kelly Robson <kellyalvesrobson@gmail.com>
2025-10-14 14:40:32 -0300

    add new methods to users router. get and post

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Response,HTTPException
+from schemas import UserIn,UserOut
 
+users = []
 
 
 app = FastAPI()
 
-@app.get("/users")
-async def add_new_user():
+@app.post("/users",status_code=201)
+async def add_new_user(user:UserIn):
+    users.append(user)
+    return Response(content="New recource created",status_code=201)
+
+
+@app.get("/users",status_code=200)
+async def get_all_users():
+    if len(users) >0:
+        # return HTTPException(status_code=204)
+        return Response(status_code=204,content="No content")
+    return users
     
