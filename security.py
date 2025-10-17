62bdf3009e6b775137b536acea60805790c51de7
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2025-10-17 15:31:54 -0300

    create function access token

--- security.py
+++ security.py
@@ -1,12 +1,11 @@
 from passlib.hash import bcrypt
 from passlib.hash import bcrypt
+import jwt
+from datetime import datetime,timedelta,timezone
 
-h = bcrypt.hash("password")
-h = bcrypt.hash("password")
-
-print(h)
-
-
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = "HS256"
+SECRET_KEY = ""
 
 def hash_password(password:str):
     hashed_password = bcrypt.hash(password)
@@ -14,3 +13,21 @@ def hash_password(password:str):
 
 def verifify_password(pwd,hashed_pwd):
     return bcrypt.verify(pwd,hashed_pwd)
+
+def create_token(user_id,duration_token=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    expiration_date = datetime.now(timezone.utc) + duration_token
+    dic_info ={
+        "sub":user_id,
+        "expiration_date":str(expiration_date)
+    } 
+
+    print(dic_info)
+    encoded_jwt = jwt.encode(dic_info,key=SECRET_KEY,algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+
+    
+
+if __name__ == "__main__":
+    print(create_token({"fala comigo":"Marcolas"}))
