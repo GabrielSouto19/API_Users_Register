d628ef77073f99069371057acb2aa07d3002b522
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2025-10-17 16:55:11 -0300

    finish function create access token

--- security.py
+++ security.py
@@ -1,11 +1,14 @@
 from passlib.hash import bcrypt
-from passlib.hash import bcrypt
 import jwt
 from datetime import datetime,timedelta,timezone
+from db import UserDB,get_session
+from fastapi import Depends,HTTPException
+from sqlalchemy.orm import Session 
+
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 ALGORITHM = "HS256"
-SECRET_KEY = ""
+SECRET_KEY = "meu_segredo_muito_bom"
 
 def hash_password(password:str):
     hashed_password = bcrypt.hash(password)
@@ -14,20 +17,16 @@ def hash_password(password:str):
 def verifify_password(pwd,hashed_pwd):
     return bcrypt.verify(pwd,hashed_pwd)
 
-def create_token(user_id,duration_token=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
-    expiration_date = datetime.now(timezone.utc) + duration_token
-    dic_info ={
+
+def create_acess_token(user_id:int,expire_minutes:int=ACCESS_TOKEN_EXPIRE_MINUTES)-> str:
+    expire = datetime.now(timezone.utc) + timedelta(minutes=expire_minutes)
+    payload = {
         "sub":user_id,
-        "expiration_date":str(expiration_date)
-    } 
-
-    print(dic_info)
-    encoded_jwt = jwt.encode(dic_info,key=SECRET_KEY,algorithm=ALGORITHM)
-
-    return encoded_jwt
-
-
-    
+        "exp":expire.timestamp(),
+        "iat": datetime.now(timezone.utc).timestamp()
+    }
+    token = jwt.encode(payload,SECRET_KEY,ALGORITHM)
+    return token 
 
 if __name__ == "__main__":
-    print(create_token({"fala comigo":"Marcolas"}))
+    print(create_acess_token(1))
